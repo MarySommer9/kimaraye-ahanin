afef31cb12f6fa902eaf039c05cf474426746121
MarySommer9 <marysommer2004@gmail.com>
2026-08-10 19:46:34 +0330

    Delete kimaraye-ahanin-main/kimaraye-ahanin-main directory

--- kimaraye-ahanin-main/kimaraye-ahanin-main/src/core/security/tls-fingerprint.ts
+++
@@ -1,1 +0,0 @@
-﻿export function getTLSFingerprint() { /* to be implemented */ }
--- kimaraye-ahanin-main/kimaraye-ahanin-main/src/core/subscription/index.ts
+++
@@ -1,7 +0,0 @@
-// ============================================
-// 🦁 کیمارای آهنین - صادرات ساب‌اسکریپشن
-// ============================================
-
-export { generateSingbox } from './singbox';
-export { generateClash } from './clash';
-export { generateV2Ray } from './v2ray';
